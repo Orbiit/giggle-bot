b1d7e5b13a60a551f67ef0afbbf27ec49b312206
SheepTester <ussa.say@gmail.com>
2018-04-04 18:20:18 -0700

    few more commands etc etc

--- main.js
+++ main.js
@@ -13,13 +13,16 @@ const left = "⬅";
 const right = "➡";
 const up = "🔼";
 const down = "🔽";
+const tree = "🌳";
 const colour = 0x00BCD4;
 
 const maxWordsPerPage = 10;
 
 let paginations = [],
 paginationData = {},
-externalEchoChannel = null;
+externalEchoChannel = null,
+reactTarget = null,
+emojiInfos = {};
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,18 +30,17 @@ client.on("ready", () => {
 
 client.on("message", msg => {
   if (msg.author.id === client.user.id) return;
-  if (/\b(hate|hated|hates|hating)\b/i.test(msg.content)) {
+  if (/\b(hate|hated|hates|hating|hatred|hater|haters)\b/i.test(msg.content)) {
+    let hat = {h: "l", H: "L", a: "o", A: "O", t: "v", T: "V"};
     msg.channel.send(`hey hey hey <@${msg.author.id}> don't be so negative! try this:`
-      + "```"
-      + msg.content
-        .replace(/hate/g, "love")
-        .replace(/hated/g, "loved")
-        .replace(/hates/g, "loves")
-        .replace(/hating/g, "loving")
-      + "```");
+      + "```" + msg.content.replace(
+        /\b(hat)(e(s|d|rs?)?|ing)\b/gi,
+        (m, c1, c2) => c1.split("").map(l => hat[l]).join("") + c2
+      ).replace(/hatred/g, "love") + "```");
     msg.react(thumbs_down);
-  } else if (msg.mentions.users.has(client.user.id)) {
-    if (/\b(help|(your *)?commands?)\b/i.test(msg.content)) {
+  } else if (msg.mentions.users.has(client.user.id) || /^moofy,? */.test(msg.content)) {
+    let sendOK = true;
+    if (/\b(help|((your|ur) *)?commands?)\b/i.test(msg.content)) {
       let content = [];
       msg.channel.send({
         embed: {
@@ -46,7 +48,7 @@ client.on("message", msg => {
             text: `good luck!`
           },
           description: Object.keys(commands).map(c => {
-            return `**\`${c}\`** ${commands[c]}`;
+            return `**\`${c.replace(/TREE/g, tree)}\`** ${commands[c].replace(/TREE/g, tree)}`;
           }).join("\n\n"),
           title: "command list",
           color: colour
@@ -66,9 +68,34 @@ client.on("message", msg => {
         }
       });
     } else {
-      msg.channel.send(`<@${msg.author.id}> DON'T MENTION ME YET`);
+      let deleteRegex = /\bdelete *([0-9]+)\b/i.exec(msg.content),
+      react = /\breact *(\S{1,2})/i.exec(msg.content);
+      if (deleteRegex) {
+        if (Math.floor(Math.random() * 4)) {
+          msg.channel.send(`<@${msg.author.id}> nahhh`);
+          sendOK = false;
+          msg.react(thumbs_down);
+        } else {
+          msg.channel.fetchMessages({limit: +deleteRegex[1]}).then(msgs => {
+            msgs.map(msg => {
+              if (msg.author.id === client.user.id) msg.delete();
+            });
+          });
+        }
+      } else if (react) {
+        try {
+          if (!reactTarget) throw new Error("which message?");
+          if (react[1] === thumbs_up || react[1] === thumbs_down)
+            throw new Error("don't dare you try to manipulate votes!");
+          reactTarget.react(react[1]);
+        } catch (e) {
+          msg.channel.send(`<@${msg.author.id}> **\`\`\`${e.toString().toUpperCase()}\`\`\`**`);
+        }
+      } else {
+        msg.channel.send(`<@${msg.author.id}> DON'T MENTION ME YET`);
+      }
     }
-    msg.react(ok);
+    if (sendOK) msg.react(ok);
   } else if (/\bpag(e|ination) *test\b/i.test(msg.content)) {
     let embed = new Discord.RichEmbed({
       footer: {
@@ -106,20 +133,41 @@ client.on("message", msg => {
   } else if (/\bdumb\b/i.test(msg.content) && /\bbot\b/i.test(msg.content)) {
     msg.channel.send(`DID I JUST HEAR "dumb" AND "bot" USED TOGETHER??!!??!11!?1/!?`);
     msg.react(thumbs_down);
+  } else if (/\binspect *emoji\b/i.test(msg.content)) {
+    let embed = new Discord.RichEmbed({
+      footer: {
+        text: `react to set emoji`
+      },
+      description: "awaiting reaction",
+      title: "emoji inspector",
+      color: colour
+    });
+    msg.channel.send({
+      embed: embed
+    }).then(msg => {
+      emojiInfos[msg.id] = {
+        embed: embed,
+        msg: msg
+      };
+    });
+    msg.react(ok);
+  } else if (/\bkeepInventory\b/i.test(msg.content) && msg.author.username === "Gamepro5") {
+    msg.channel.send(`<@${msg.author.id}>` + " make sure you set `keepInventory` to `false` :)");
+    msg.react(thumbs_down);
   } else {
-    let echo = /echo(c?)(e?)(s?):([^]*)/im.exec(msg.content),
+    let echo = /echo(c?)(e?)(s?):([^]+)/im.exec(msg.content),
     // c - enclose in codeblock; e - external; s - don't trim ("strict")
     ofNotHaveRegex = /\b(could|might|should|will|would)(?:'?ve| +have)\b/gi,
     ofNotHave = ofNotHaveRegex.exec(msg.content);
     if (echo) {
       let circumfix = echo[1] ? "```" : "",
-      content = echo[3] ? echo[4] : echo[4].trim();
+      content = (echo[3] ? echo[4] : echo[4].trim()) || "/shrug";
       ((echo[2] ? externalEchoChannel : null) || msg.channel)
         .send(circumfix + content + circumfix);
     } else if (ofNotHave) {
       msg.channel.send(
         `<@${msg.author.id}> no it's` + "```"
-        + msg.content.replace(ofNotHaveRegex, "$1 of") + "```"
+        + msg.content.replace(ofNotHaveRegex, "$1 OF") + "```"
       );
       msg.react(thumbs_down);
     }
@@ -138,24 +186,28 @@ function updatePagination(page) {
   page.embed.setDescription("```" + content + "```");
   page.msg.edit(page.embed);
 }
-function messageReactionUpdate(reaction, user) {
-  if (client.user.id === user.id) return;
-  let id = reaction.message.id;
-  if (~paginations.indexOf(id)) {
-    let pages = Math.ceil(words.length / maxWordsPerPage);
-    if (reaction.emoji.name === left && paginationData[id].page > 0)
-      paginationData[id].page--, paginationData[id].cursor = 0;
-    else if (reaction.emoji.name === right && paginationData[id].page < pages - 1)
-      paginationData[id].page++, paginationData[id].cursor = 0;
-    else if (reaction.emoji.name === up && paginationData[id].cursor > 0)
-      paginationData[id].cursor--;
-    else if (reaction.emoji.name === down) {
+function messageReactionUpdate(reaction, id) {
+  let pages = Math.ceil(words.length / maxWordsPerPage);
+  switch (reaction) {
+    case left:
+      if (paginationData[id].page > 0)
+        paginationData[id].page--, paginationData[id].cursor = 0;
+      break;
+    case right:
+      if (paginationData[id].page < pages - 1)
+        paginationData[id].page++, paginationData[id].cursor = 0;
+      break;
+    case up:
+      if (paginationData[id].cursor > 0)
+        paginationData[id].cursor--;
+      break;
+    case down:
       let max = maxWordsPerPage;
       if (paginationData[id].page === pages - 1) max = words.length % maxWordsPerPage;
       if (paginationData[id].cursor < max - 1)
         paginationData[id].cursor++;
-    }
-    else if (reaction.emoji.name === ok) {
+      break;
+    case ok:
       paginationData[id].embed.setFooter("selected");
       paginationData[id].msg.edit(paginationData[id].embed);
 
@@ -168,13 +220,33 @@ function messageReactionUpdate(reaction, user) {
 
       delete paginationData[id];
       paginations.splice(paginations.indexOf(id), 1);
-      return;
-    }
-    updatePagination(paginationData[id]);
+      return true;
+    default:
+      return false;
   }
+  updatePagination(paginationData[id]);
+  return true;
 }
 
-client.on("messageReactionAdd", messageReactionUpdate);
-client.on("messageReactionRemove", messageReactionUpdate);
+client.on("messageReactionAdd", (reaction, user) => {
+  if (client.user.id === user.id) return;
+  let id = reaction.message.id;
+  if (emojiInfos[id]) {
+    emojiInfos[id].embed.setDescription(
+      `**id**: \`${reaction.emoji.id}\`\n`
+      + `**identifier**: \`${reaction.emoji.identifier}\`\n`
+      + `**name**: \`${reaction.emoji.name}\``
+    );
+    emojiInfos[id].embed.setFooter("selected");
+    emojiInfos[id].msg.edit(emojiInfos[id].embed);
+    delete emojiInfos[id];
+  } else if (reaction.emoji.name === tree) {
+    reactTarget = reaction.message;
+  } else if (~paginations.indexOf(id) && messageReactionUpdate(reaction.emoji.name, id));
+});
+client.on("messageReactionRemove", (reaction, user) => {
+  let id = reaction.message.id;
+  if (~paginations.indexOf(id) && messageReactionUpdate(reaction.emoji.name, id));
+});
 
 client.login(Token.token);
